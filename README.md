dea7485c1b276725ab544710258e816894893dad
Ivan D Vasin <nisavid@gmail.com>
2013-10-11 16:58:59 -0400

    first public version

--- README.md
+++
@@ -1,4 +0,0 @@
-py-logging
-==========
-
-Logging.
---
+++ nisavid/logging/__init__.py
@@ -0,0 +1,12 @@
+"""Logging.
+
+This is a wrapper and extension of :mod:`logging`.  Use it instead of
+:mod:`logging`.  Mixing the two will work, but there is no need, since
+this module provides a superset of the :mod:`logging` API.
+
+"""
+
+__copyright__ = "Copyright (C) 2013 Ivan D Vasin and Cogo Labs"
+__docformat__ = "restructuredtext"
+
+from ._core import *
